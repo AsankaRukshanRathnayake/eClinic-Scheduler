94823fa25af4ad71c19254c04d17b83887231bfb
AsankaRukshanRathnayake <rathnay-se19053@stu.kln.ac.lk>
2023-09-11 23:25:59 +0530

    Added index page

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+    <head>
+
+    </head>
+    <body>
+        <header>
+            <h1>MyClinic</h1>
+
+            <button onclick="redirect('Patient/login.php')">Login</button>
+            <button onclick="redirect('Patient/register.php')">Register</button>
+        </header>
+
+        <footer>
+            Created by Asanka Rukshan Rathnayake
+        </footer>
+
+        <script src="js/script.js"></script>
+    </body>
+</html>
