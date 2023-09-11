d5d56278af3d23073ccd543ce9f703b8414629ad
AsankaRukshanRathnayake <rathnay-se19053@stu.kln.ac.lk>
2023-09-11 23:27:37 +0530

    Added patient home page

---
+++ Patient/home.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+    <head>
+
+    </head>
+    <body>
+        <div class="sidenavbar">
+            <?php include 'navbar.php'; ?>
+        </div>
+
+        <div class="main">
+            <form action="">
+                <input type="search" placeholder="search">
+            </form>
+
+            <table>
+                <tr>
+                    <td>01 Sep</td>
+                    <td>9.00am</td>
+                    <td>Dental Clinic</td>
+                    <td><button>Cancel</button></td>
+                    <td><button>Reschedule</button></td>
+                </tr>
+
+                <tr>
+                    <td>01 Sep</td>
+                    <td>9.00am</td>
+                    <td>Dental Clinic</td>
+                    <td><button>Cancel</button></td>
+                    <td><button>Reschedule</button></td>
+                </tr>
+
+                <tr>
+                    <td>01 Sep</td>
+                    <td>9.00am</td>
+                    <td>Dental Clinic</td>
+                    <td><button>Cancel</button></td>
+                    <td><button>Reschedule</button></td>
+                </tr>
+            </table>
+        </div>
+
+        <script src="../js/script.js"></script>
+    </body>
+</html>
