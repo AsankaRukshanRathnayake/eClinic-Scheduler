1ff402571281f7ba35ef7d37aaa582d4e9c236e2
AsankaRukshanRathnayake <rathnay-se19053@stu.kln.ac.lk>
2023-09-11 23:27:10 +0530

    Added patient login form

---
+++ Patient/login.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+    <head>
+
+    </head>
+    <body>
+        <form action="home.php" method="POST">
+            <h1>Login</h1>
+
+            <label for="patientID">PatientID</label>
+            <input type="text" name="patientID" placeholder="1234-5678-9123" required>
+
+            <label for="password">Password</label>
+            <input type="password" name="password" placeholder="xxxxxxxx" required>
+
+            <input type="submit" value="Submit">
+        </form>
+    </body>
+</html>
