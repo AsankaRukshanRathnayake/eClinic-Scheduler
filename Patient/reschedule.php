6bf7d936011d3c596bdf06fa3972963caf6424ab
AsankaRukshanRathnayake <rathnay-se19053@stu.kln.ac.lk>
2023-09-12 18:32:22 +0530

    Added appointment rescheduling page

---
+++ Patient/reschedule.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="../css/styles.css">
+    </head>
+    <body>
+        <div class="sidenavbar">
+            <?php include 'navbar.php'; ?>
+        </div>
+
+        <div class="main">
+            <table>
+                <tr>
+                    <td>01 Sep</td>
+                    <td>9.00am</td>
+                    <td>Full</td>
+                    <td><button>Make Appointment</button></td>
+                </tr>
+                <tr>
+                    <td>01 Sep</td>
+                    <td>9.00am</td>
+                    <td>Full</td>
+                    <td><button>Make Appointment</button></td>
+                </tr>
+                <tr>
+                    <td>01 Sep</td>
+                    <td>9.00am</td>
+                    <td>Full</td>
+                    <td><button>Make Appointment</button></td>
+                </tr>
+            </table>
+        </div>
+
+        <script src="../js/script.js"></script>
+    </body>
+</html>
