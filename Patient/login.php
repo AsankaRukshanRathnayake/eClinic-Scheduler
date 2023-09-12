7ff8f617f2fe75f8998336f280e2d445e2ff3832
AsankaRukshanRathnayake <rathnay-se19053@stu.kln.ac.lk>
2023-09-12 18:33:47 +0530

    Minimal change

--- Patient/login.php
+++ Patient/login.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
     <head>
-
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="../css/styles.css">
     </head>
     <body>
         <form action="home.php" method="POST">
