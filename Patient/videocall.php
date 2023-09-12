a434109b6517b4396cb5575c8b0d683639b58a09
AsankaRukshanRathnayake <rathnay-se19053@stu.kln.ac.lk>
2023-09-12 18:32:35 +0530

    Added video call page

---
+++ Patient/videocall.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="../css/styles.css">
+    </head>
+    <body>
+        <div class="video">
+
+        </div>
+        <div class="videoactions">
+            <form action="">
+                <textarea name="chat" id="chat" cols="50" rows="5" placeholder>Send a Message</textarea>
+                <input type="submit" name="send" value="Send">
+            </form>
+            <button onclick="redirect('other/pending.php')">End</button>
+        </div>
+
+        <script src="../js/script.js"></script>
+    </body>
+</html>
