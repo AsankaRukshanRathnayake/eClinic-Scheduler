feec5605e876f89b5769cc4ab942e7e9b04b7ee4
AsankaRukshanRathnayake <rathnay-se19053@stu.kln.ac.lk>
2023-09-12 18:33:17 +0530

    Pending appointment schedule

---
+++ Patient/other/pending.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="../css/styles.css">
+        <meta http-equiv="refresh" content="15,url=../home.php">
+    </head>
+    <body>
+        <p>Your request is pending</p>
+        <p>Once your appointment is placed, you may see it in your appointments section.</p>
+        <p>We will send notfications via this site's notfication facility and email as well.</p>
+
+        <p>You will be redirected to the home page shortly</p>
+    </body>
+</html>
