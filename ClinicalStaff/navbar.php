8ea1196d1bdde9406e45cb44ffc086c70a1fcd82
AsankaRukshanRathnayake <rathnay-se19053@stu.kln.ac.lk>
2023-09-13 21:07:13 +0530

    Added clinical staff dashboard

---
+++ ClinicalStaff/navbar.php
@@ -0,0 +1,14 @@
+<div>
+    <p>ClinicalStaff1</p>
+    <p>123-456</p>
+    <button onclick="redirect('logout.php')">Logout</button>
+</div>
+
+<div>
+    <button onclick="redirect('home.php')">Home</button><br>
+    <button onclick="redirect('registerPatient.php')">Register<br>Patient</button>
+</div>
+
+<div>
+    <button onclick="redirect('notifications.php')">Notifications</button>
+</div>
