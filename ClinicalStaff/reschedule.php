ab3d7b1600dca9f6a944f9ba555c94abc381ee1c
AsankaRukshanRathnayake <rathnay-se19053@stu.kln.ac.lk>
2023-09-13 21:09:43 +0530

    Added clinic rescheduling page

---
+++ ClinicalStaff/reschedule.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="../css/styles.css">
+    </head>
+    <body>
+        <div class="sidenavbar">
+            <?php include 'navbar.php'; ?>
+        </div>
+
+        <div class="main">
+            <form action="home.php" method="POST">
+                <input type="hidden" name="clinicID">
+
+                <label for="newdate">New Date</label>
+                <input type="date" name="newdate"><br><br>
+
+                <label for="newtime">Time</label>
+                <input type="time" name="newtime" value="09:00"><br><br>
+
+                <label for="newvenue">Venue</label>
+                <input type="text" name="newvenue"><br><br>
+
+                <label for="chiefdoctor">Doctor</label>
+                <select name="chiefdoctor" id="">
+                    <option value="Mr. Perera">Mr. Perera</option>
+                    <option value="Mr. Gamage">Mr. Gamage</option>
+                    <option value="Mr. Rahman">Mr. Rahman</option>
+                </select><br><br>
+
+                <input type="reset" value="reset">
+                <button onclick="redirect('home.php')">Cancel</button>
+                <input type="submit" name="submit" id="" value="Reschedule">
+            </form>
+        </div>
+
+        <script src="../js/script.js"></script>
+    </body>
+</html>
