fa3d694bcf4895169ef79eed8ef1f519af4ed895
AsankaRukshanRathnayake <rathnay-se19053@stu.kln.ac.lk>
2023-09-14 12:45:34 +0530

    Added OPD  doctors dashbord page

---
+++ Doctor/home.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+    <head>
+        <title>Home</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="../css/styles.css">
+    </head>
+    <body>
+        <div class="sidenavbar">
+            <?php include 'navbar.php'; ?>
+        </div>
+
+        <div class="main">
+            <div class="topnavbar">        
+                <form action="">
+                    <input type="search" placeholder="search for a clinic">
+                </form>
+            </div>
+
+            <table>
+                <tr>
+                    <td>01 Sep</td>
+                    <td>9.00am</td>
+                    <td>Dr. Perera</td>
+                    <td>23 Patients</td>
+                    <td><button>Postpone</button></td>
+                    <td><button onclick="redirect('reschedule.php')">Reschedule</button></td>
+                </tr>
+
+                <tr>
+                    <td>01 Sep</td>
+                    <td>9.00am</td>
+                    <td>Dr. Perera</td>
+                    <td>23 Patients</td>
+                    <td><button>Postpone</button></td>
+                    <td><button onclick="redirect('reschedule.php')">Reschedule</button></td>
+                </tr>
+
+                <tr>
+                    <td>01 Sep</td>
+                    <td>9.00am</td>
+                    <td>Dr. Perera</td>
+                    <td>23 Patients</td>
+                    <td><button>Postpone</button></td>
+                    <td><button onclick="redirect('reschedule.php')">Reschedule</button></td>
+                </tr>
+            </table>
+        </div>
+
+        <script src="../js/script.js"></script>
+    </body>
+</html>
