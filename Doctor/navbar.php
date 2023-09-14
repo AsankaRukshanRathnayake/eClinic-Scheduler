3941a9717a006030c701e27bfcb5d5fbc87cbeda
AsankaRukshanRathnayake <rathnay-se19053@stu.kln.ac.lk>
2023-09-14 12:46:30 +0530

    Side navigation bar to the Doctor's dashboards

---
+++ Doctor/navbar.php
@@ -0,0 +1,15 @@
+<div>
+    <p>Doctor1</p>
+    <p>DC123-456</p>
+    <button onclick="redirect('logout.php')">Logout</button>
+</div>
+
+<div>
+    <button onclick="redirect('home.php')">Home</button><br>
+    <button onclick="redirect('videocall.php')">Answer<br>Call</button><br>
+    <button onclick="redirect('externalrequests.php')">Requests/<br>Recommendations</button><br>
+</div>
+
+<div>
+    <button onclick="redirect('notifications.php')">Notifications</button>
+</div>
