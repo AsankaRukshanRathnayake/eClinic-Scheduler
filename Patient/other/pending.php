0f51a1b21421ed3717b0837933fd1213cbfefea2
AsankaRukshanRathnayake <rathnay-se19053@stu.kln.ac.lk>
2023-09-19 23:49:49 +0530

    Minimal change

--- Patient/other/pending.php
+++ Patient/other/pending.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
     <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
         <link rel="stylesheet" href="../../css/styles.css">
         <meta http-equiv="refresh" content="15,url=../home.php">
     </head>
