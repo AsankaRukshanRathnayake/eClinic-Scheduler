9d9614f0724c81f91268ac16f56d35ce68a072e8
AsankaRukshanRathnayake <88618141+AsankaRukshanRathnayake@users.noreply.github.com>
2023-09-20 07:13:24 +0530

    Modified login page

--- Patient/login.php
+++ Patient/login.php
@@ -3,17 +3,19 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="../css/styles.css">
     </head>
-    <body>
-        <form action="home.php" method="POST" class="patientform">
-            <h1>Login</h1>
+    <body style="background-image: url('../images/background.jpg');">
+        <div class="login">
+            <form action="home.php" method="POST" class="">
+                <h1>Login</h1>
 
-            <label for="patientID">PatientID</label><br>
-            <input type="text" name="patientID" placeholder="1234-5678-9123" required><br><br>
+                <label for="patientID">PatientID</label><br>
+                <input type="text" name="patientID" placeholder="1234-5678-9123" required><br><br>
 
-            <label for="password">Password</label><br>
-            <input type="password" name="password" placeholder="xxxxxxxx" required><br><br>
+                <label for="password">Password</label><br>
+                <input type="password" name="password" placeholder="xxxxxxxx" required><br><br>
 
-            <input type="submit" value="Submit">
-        </form>
+                <input type="submit" value="Submit" class="submit">
+            </form>
+        </div>
     </body>
 </html>
